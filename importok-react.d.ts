e201b82b9b351c2450cfe7e35cca8349483b6cad
Iacovos Constantinou <softius@gmail.com>
2024-07-17 09:26:08 +0300

    Improve support for React + TypeScript

--- importok-react.d.ts
+++ importok-react.d.ts
@@ -1,9 +1,34 @@
 import MainElement from '@importok/javascript/MainElement';
+import * as React from 'react';
 
-declare module '@importok/react' {
-    import * as React from 'react';
+export interface ImportokWizardProps {
+  title?: string;
+  subtitles?: string[];
+  fields: {
+    [key: string]: Omit<ImportField, 'description'> & {
+      description?: string;
+    };
+  };
+  transformers?: {
+    [key: string]: (record: ImportRecord, key: string, ...args: string[]) => boolean | string;
+  };
+  validators?: {
+    [key: string]: (record: ImportRecord, key: string, ...args: string[]) => boolean | string;
+  };
+  providers?: {
+    [key: string]: ProviderCallback;
+  };
+  mapper?: MapperStrategy;
+  sampleFile?: string;
+  uploadedFile?: File;
+  throttle?: false | number;
+  meta?: any;
+  onRecordReady?: (record: ImportRecord, meta: any) => any;
+  onImportReady?: (records: ImportRecord[], meta: any) => any;
+}
 
-    export class ImportokWizard extends React.Component {
-        wizard: MainElement;
-    }
+export interface ImportokWizardState {}
+
+export default class ImportokWizard extends React.Component<ImportokWizardProps, ImportokWizardState> {
+  wizard: MainElement;
 }
